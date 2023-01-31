f9132c61d52c076e64dbe2b0f6b939d4f7fcb953
Rolandsson <102300415+Rolandsson@users.noreply.github.com>
2023-01-31 10:23:43 +0100

    Add files via upload

---
+++ live-coding/vue-fetch/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import SearchView from './components/SearchView.vue';
+</script>
+
+<template>
+  <div class="container">
+    <SearchView />
+  </div>
+</template>
+
+
+<style scoped>
+  .container {
+    width: 40%;
+    margin: 0 auto;
+    margin-top: 10%;
+    background-color: rgba(255, 255, 255, 0.3);
+    border-radius: .7em;
+    padding: 2em;
+  }
+</style>
